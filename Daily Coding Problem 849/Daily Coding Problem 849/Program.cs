d3670176ea648e6a6efb8d4ed833d4ae517ed0c5
Alan Roach <arscience97@gmail.com>
2021-04-20 18:10:59 -0400

    Changed method to get correct number of steps.

--- Daily Coding Problem 849/Daily Coding Problem 849/Program.cs
+++ Daily Coding Problem 849/Daily Coding Problem 849/Program.cs
@@ -7,10 +7,6 @@
 //Bonus: What input n <= 1000000 gives the longest sequence?
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Daily_Coding_Problem_849
 {
@@ -19,34 +15,35 @@ namespace Daily_Coding_Problem_849
         static void Main(string[] args)
         {
             //Uncomment below if using from IDE and not EXE.
-            //Console.Write("Set starting value: ");
-            //double answer = Double.Parse(Console.ReadLine());
-            //Console.WriteLine(collatzSeq(answer));
-            
+            Console.Write("Set starting value: ");
+            double answer = Double.Parse(Console.ReadLine());
+            Console.WriteLine(collatzSeq(answer));
+
             //Comment out below line if using from IDE and not EXE
-            Console.WriteLine(collatzSeq(Double.Parse(args[0])));
-            
+            //Console.WriteLine(collatzSeq(Double.Parse(args[0])));
+
             Console.ReadKey();
         }
 
         static string collatzSeq(double num)
         {
-            int runNum = 1;
-            while (num != 1)
+            int runNum=0;
+            do
             {
-                Console.WriteLine("Value of num at start: " + num);
+                //Console.WriteLine("Value of num at start: " + num);
                 if (num % 2 == 0)
                 {
                     num = num / 2;
                 }
-                else if (num % 2 != 0)
+                else
                 {
                     num = (3 * num) + 1;
                 }
                 runNum++;
-                Console.WriteLine("Value of num at end: " + num);
+                //Console.WriteLine("Value of num at end: " + num);
 
-            }
+
+            } while (num != 1);
             return "Success, number of steps: " + runNum;
         }
     }
